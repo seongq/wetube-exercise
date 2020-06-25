c30f9552c72e4b607d4fc910b9667354bc2fbce6
seongq <36037178+seongq@users.noreply.github.com>
2020-06-26 08:03:48 +0900

    #2.6 Express Core:Middlewares
    nodemon
    middle ware concepts

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express";
+// const express = require("express");
+const app = express();
+
+const PORT = 4000;
+
+const handleListening = () =>
+  console.log(`listening on : http://localhost:${PORT}`);
+
+const handleHome = (req, res) => res.send("Hello from ASS");
+
+const handleProfile = (req, res) => res.send("You are on my profile");
+
+const betweenHome = (req, res, next) => {
+  console.log("I'm between ");
+  next();
+};
+app.use(betweenHome);
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
+
+app.listen(PORT, handleListening);
