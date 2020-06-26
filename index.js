bd4a358b636039b49e67efa6f07b7042d2874044
seongq <36037178+seongq@users.noreply.github.com>
2020-06-27 04:39:13 +0900

    #2.7 Express Core: MIddlewares part Two

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import express from "express";
-// const express = require("express");
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 const app = express();
 
 const PORT = 4000;
@@ -11,11 +14,12 @@ const handleHome = (req, res) => res.send("Hello from ASS");
 
 const handleProfile = (req, res) => res.send("You are on my profile");
 
-const betweenHome = (req, res, next) => {
-  console.log("I'm between ");
-  next();
-};
-app.use(betweenHome);
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
+
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
 
