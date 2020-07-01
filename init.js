5d8af989c01d08f1b17401d1cab36263c6a43c52
seongq <36037178+seongq@users.noreply.github.com>
2020-07-01 15:23:44 +0900

    #3.3,#3.4
    #3.3 Video Model
    #3.4 Comment Model

--- init.js
+++ init.js
@@ -2,6 +2,8 @@ import app from "./app";
 import "./db";
 import dotenv from "dotenv";
 dotenv.config();
+import "./models/Comment";
+import "./models/Video";
 
 const PORT = process.env.PORT || 4000;
 
