adc2786a449c14e692228b2378ba0b217c80a400
seongq <36037178+seongq@users.noreply.github.com>
2020-07-04 19:19:14 +0900

    #3.10 Deleting a Video

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -41,10 +41,9 @@ export const videoDetail = async (req, res) => {
   const {
     params: { id },
   } = req;
-  console.log(id);
   try {
     const video = await Videos.findById(id);
-    res.render("videoDetail", { pageTitle: "Video Detail", video });
+    res.render("videoDetail", { pageTitle: video.title, video });
   } catch (error) {
     res.redirect(routes.home);
   }
@@ -68,12 +67,19 @@ export const postEditVideo = async (req, res) => {
     body: { title, description },
   } = req;
   try {
-    await Videos.findeOneAndUpdate({ id }, { title, description });
+    await Videos.findeOneAndUpdate({ _id: id }, { title, description });
     res.redirect(routes.videoDetail(id));
   } catch (error) {
     res.redirect(routes.home);
   }
 };
 
-export const deleteVideo = (req, res) =>
-  res.render("deleteVideo", { pageTitle: "Delete Video" });
+export const deleteVideo = async (req, res) => {
+  const {
+    params: { id },
+  } = req;
+  try {
+    await Videos.findOneAndRemove({ _id: id });
+  } catch (error) {}
+  res.redirect(routes.home);
+};
